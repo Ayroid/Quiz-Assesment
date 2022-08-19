b4cf12ebfaf1c2f90f09d9035b7d47637fc8e2d1
NIKHIL SINGH <90443399+Nikhilbisht17@users.noreply.github.com>
2022-08-19 11:05:54 +0530

    Add files via upload

---
+++ add.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(){
+    int a,b;
+    printf("Enter first number: ");
+    scanf("%d", &a);
+    printf("Enter second number: ");
+    scanf("%d5", &b);
+    int sum=a+b;
+    printf("sum of the digits is: %d",sum);
+    return 0;
+}
