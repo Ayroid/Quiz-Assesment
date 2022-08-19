7d72a3d6b133f1572c44edd9d78c1f342c58e501
Ayroid <ayushsk0000@gmail.com>
2022-08-19 13:46:23 +0530

    Added Questions in the Questions File

--- QuizEvaluation.java
+++ QuizEvaluation.java
@@ -2,7 +2,7 @@ import java.io.*;
 import java.util.*;
 class QuizEvaluation{
     public static void main(String[] args) {
-        char arr[]={'a','b'};
+        char arr[]={'b','b','b','d','d','d','c','c','b','a','c','c','a','b','c','b','a','a','b','a'};
         int i=0, ctr=0;
         try {
             File Obj = new File("Questions.txt");
@@ -12,6 +12,7 @@ class QuizEvaluation{
                 String data = Reader.nextLine();
                 if(data.startsWith(" ")){
                     char choice;
+                    System.out.print("\nYour Answer: ");
                     choice=sc.next().charAt(0);
                     if(choice==arr[i]){
                         ctr++;
