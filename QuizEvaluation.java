20379c66afb0d0905f52c29bdc7dade693a11e9c
Ayroid <ayushsk0000@gmail.com>
2022-08-19 14:16:42 +0530

    Modified the Questions File and Added a starting interface for user

--- QuizEvaluation.java
+++ QuizEvaluation.java
@@ -2,13 +2,16 @@ import java.io.*;
 import java.util.*;
 class QuizEvaluation{
     public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("\033[H\033[2J"); // Escape Sequence to Clear Terminal Screen
+        System.out.println("\n\n\n\t\t\t\t\t\t\t\t\t\t\tJAVA QUIZ\n Enter a number to Start!");
+        int x=sc.nextInt();
         char arr[]={'b','b','b','d','d','d','c','c','b','a','c','c','a','b','c','b','a','a','b','a'};
         int i=0, ctr=0;
         try {
-            File Obj = new File("Questions.txt");
-            Scanner sc=new Scanner(System.in);
-            Scanner Reader = new Scanner(Obj);
             System.out.print("\033[H\033[2J"); // Escape Sequence to Clear Terminal Screen
+            File Obj = new File("Questions.txt");
+            Scanner Reader = new Scanner(Obj);
             while (Reader.hasNextLine()) {
                 String data = Reader.nextLine();
                 if(data.startsWith(" ")){
