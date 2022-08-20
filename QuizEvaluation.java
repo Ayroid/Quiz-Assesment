e40a18f896ccbe3874ceba91bc487bd12aabe5e2
Ayroid <ayushsk0000@gmail.com>
2022-08-20 13:03:10 +0530

    1. Added answers in question file 2. Made changes so that answers can be verified

--- QuizEvaluation.java
+++ QuizEvaluation.java
@@ -6,20 +6,20 @@ class QuizEvaluation{
         System.out.print("\033[H\033[2J"); // Escape Sequence to Clear Terminal Screen
         System.out.println("\n\n\n\t\t\t\t\t\t\t\t\t\t\tJAVA QUIZ\n Enter a number to Start!");
         int x=sc.nextInt();
-        char arr[]={'b','b','b','d','d','d','c','c','b','a','c','c','a','b','c','b','a','a','b','a'};
         int i=0, ctr=0;
         try {
             System.out.print("\033[H\033[2J"); // Escape Sequence to Clear Terminal Screen
             String xx="4";
             File Obj = new File(xx+".txt");
             Scanner Reader = new Scanner(Obj);
+            String answers=Reader.nextLine();
             while (Reader.hasNextLine()) {
                 String data = Reader.nextLine();
                 if(data.startsWith(" ")){
                     char choice;
                     System.out.print("\nYour Answer: ");
                     choice=sc.next().charAt(0);
-                    if(choice==arr[i]){
+                    if(choice==answers.charAt(i)){
                         ctr++;
                     }
                     i++;
